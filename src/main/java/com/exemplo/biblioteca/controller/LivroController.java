897ba0f3004f2b8e7332845780aeb70c8c46f388
RoneyVasconcelos <112006151+RoneyVasconcelos@users.noreply.github.com>
2024-06-05 20:05:28 -0300

    123

--- src/main/java/com/exemplo/biblioteca/controller/LivroController.java
+++ src/main/java/com/exemplo/biblioteca/controller/LivroController.java
@@ -1,32 +1,26 @@
 package com.exemplo.biblioteca.controller;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class LivroController {
 
-    /**
-     * @param model
-     * @return
-     */
     @GetMapping("/livros")
-    public String getLivros(Model model) {
-        String url = "https://cadastro-livros.onrender.com/biblioteca/pesquisar_livro/";
+    public String getLivros(Model model) throws IOException, ParseException {
+        String url = "https://cadastro-livros.onrender.com/biblioteca/listar_livros/";
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet request = new HttpGet(url);
             try (CloseableHttpResponse response = httpClient.execute(request)) {
@@ -34,11 +28,7 @@ public class LivroController {
                 ObjectMapper objectMapper = new ObjectMapper();
                 List<Map<String, Object>> livros = objectMapper.readValue(jsonResponse, new TypeReference<List<Map<String, Object>>>() {});
                 model.addAttribute("livros", livros);
-            } catch (IOException | ParseException e) {
-                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         return "livros";
     }
