59008c1a8ef3cbdac928e11cee065fde36b2bb10
RoneyVasconcelos <112006151+RoneyVasconcelos@users.noreply.github.com>
2024-06-05 19:53:18 -0300

    1232

--- src/main/java/com/exemplo/biblioteca/controller/LivroController.java
+++ src/main/java/com/exemplo/biblioteca/controller/LivroController.java
@@ -1,26 +1,29 @@
-package com.example.demo.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.apache.hc.core5.http.ParseException;
-
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.type.TypeReference;
+package com.exemplo.biblioteca.controller;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
 public class LivroController {
 
+    /**
+     * @param model
+     * @return
+     */
     @GetMapping("/livros")
     public String getLivros(Model model) {
         String url = "https://cadastro-livros.onrender.com/biblioteca/pesquisar_livro/";
